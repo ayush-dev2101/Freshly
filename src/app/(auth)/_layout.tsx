327fd1f20e8d28bec38658ca90a2def5b370d7b1
ayush-dev2101 <max.ayushhh@gmail.com>
2026-06-25 16:23:27 +0530

    feat(auth): integrate Clerk for authentication and user management
    - Added Clerk dependencies to package.json.
    - Created AuthRoutesLayout to handle authentication state and redirects.
    - Implemented SignInScreen for user sign-in functionality.
    - Created home layout to manage access based on authentication state.
    - Developed home page to display user information and sign-out option.
    - Updated root layout to include ClerkProvider for authentication context.
    - Removed unused index.tsx file and trash.txt.

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect, Stack } from "expo-router";
+
+export default function AuthRoutesLayout() {
+  const { isSignedIn, isLoaded } = useAuth();
+//If Clerk is not loaded wait 
+  if (!isLoaded) {
+    return null;
+  }
+  //If user is signed in
+  if (isSignedIn) {
+    return <Redirect href={"/"} />;
+  }
+  return <Stack />;
+}
